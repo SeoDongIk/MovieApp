3df27c7a69d7006ec2df1d4521b03d0c9ee392d4
SeoDongIk <ikmin7373@naver.com>
2024-11-28 23:54:21 +0900

    Apply custom theme for the app

---
+++ app/src/main/java/com/example/movieapp/ui/component/movie/Category.kt
@@ -0,0 +1,65 @@
+package com.example.movieapp.ui.component.movie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.movieapp.ui.theme.MovieAppTheme
+import com.example.movieapp.ui.theme.Paddings
+
+@Composable
+fun CategoryRow() {
+    Column() {
+        CategoryTitle("Action")
+        LazyRow(
+            contentPadding = PaddingValues(
+                horizontal = 10.dp
+            )
+        ) {
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+            item {
+                MovieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(
+    title : String
+) {
+    Text(
+        text = "Action",
+        modifier = Modifier
+            .padding(
+                vertical = Paddings.large,
+                horizontal = Paddings.extra
+            ),
+        style = MaterialTheme.typography.body1
+    )
+}
+
+@Preview
+@Composable
+fun CategoryRowPreview() {
+    MovieAppTheme {
+        CategoryRow()
+    }
+}
